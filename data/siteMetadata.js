6b898f2327cb57d7be3e78c0a4fa8712c3ed022e
Tek Loon <tekloon.1991@gmail.com>
2023-12-10 14:42:20 +0800

    feat: TEMP disable analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -10,7 +10,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
+  // mastodon: 'https://mastodon.social/@mastodonuser',
   // email: 'tekloon.1991@gmail.com',
   github: 'https://github.com/tlcheah2',
   // twitter: 'https://twitter.com/Twitter',

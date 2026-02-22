1960a1b35ac19f2098fe8413aed08b35314f3bf8
Tek Loon <tl@powertothebrand.sg>
2026-02-22 22:33:30 +0800

    fix: ignore eslint during build

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -128,6 +128,41 @@ export const Authors = defineDocumentType(() => ({
   computedFields,
 }))
 
+// export const Thoughts = defineDocumentType(() => ({
+//   name: 'Thoughts',
+//   filePathPattern: 'thoughts/**/*.mdx',
+//   contentType: 'mdx',
+//   fields: {
+//     title: { type: 'string', required: true },
+//     date: { type: 'date', required: true },
+//     tags: { type: 'list', of: { type: 'string' }, default: [] },
+//     lastmod: { type: 'date' },
+//     draft: { type: 'boolean' },
+//     summary: { type: 'string' },
+//     images: { type: 'json' },
+//     authors: { type: 'list', of: { type: 'string' } },
+//     layout: { type: 'string' },
+//     bibliography: { type: 'string' },
+//     canonicalUrl: { type: 'string' },
+//   },
+//   computedFields: {
+//     ...computedFields,
+//     structuredData: {
+//       type: 'json',
+//       resolve: (doc) => ({
+//         '@context': 'https://schema.org',
+//         '@type': 'BlogPosting',
+//         headline: doc.title,
+//         datePublished: doc.date,
+//         dateModified: doc.lastmod || doc.date,
+//         description: doc.summary,
+//         image: doc.images ? doc.images[0] : siteMetadata.socialBanner,
+//         url: `${siteMetadata.siteUrl}/${doc._raw.flattenedPath}`,
+//       }),
+//     },
+//   },
+// }))
+
 export default makeSource({
   contentDirPath: 'data',
   documentTypes: [Blog, Authors],
